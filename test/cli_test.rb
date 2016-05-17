1596dbc8a66eaf7246344b052903e3dd3ef173ca
Kazuyuki Honda <hakobera@gmail.com>
2016-05-17 23:17:59 +0900

    Revert "Temporary remove failed test on travis"
    This reverts commit 4bd0fa81f9af2547bc57ab293218572dec88b90e.

---
+++ test/cli_test.rb
@@ -0,0 +1,17 @@
+require_relative './test_helper'
+
+class Tumugi::Plugin::Command::CLITest < Test::Unit::TestCase
+  examples = {
+    'run_command' => ['run_command.rb', 'task1'],
+    'save_result_to_file' => ['save_result_to_file.rb', 'task1'],
+  }
+
+  def exec(file, task, options)
+    system("bundle exec tumugi run -f ./examples/#{file} #{options} #{task}")
+  end
+
+  data(examples)
+  test 'success' do |(file, task)|
+    assert_true(exec(file, task, "-w 4 --quiet"))
+  end
+end
