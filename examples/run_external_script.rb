09b76e772b1a8d98c343f3b4bad93e3dc1b023d0
Kazuyuki Honda <hakobera@gmail.com>
2016-07-09 17:26:20 +0900

    Update dependent tumugi version to >= 0.6.0 and update examples

--- examples/run_external_script.rb
+++ examples/run_external_script.rb
@@ -1,9 +1,9 @@
 task :task1, type: :command do
   requires :task2
-  param_set :command, -> { "cat #{input.path}" }
+  command { "cat #{input.path}" }
 end
 
 task :task2, type: :command do
-  param_set :command, "./examples/external_script.sh"
+  command "./examples/external_script.sh"
   output target(:local_file, "tmp/external_script_result.txt")
 end
--- examples/save_result_to_file.rb
+++ examples/save_result_to_file.rb
@@ -1,4 +1,4 @@
 task :task1, type: :command do
-  param_set :command, "echo 'success'"
-  param_set :output_file, "tmp/result.txt"
+  command "echo 'success'"
+  output_file "tmp/result.txt"
 end
